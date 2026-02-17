9a671b2ffc02e070ac05127fba38889a857030b8
matthew_hebert <matthew_hebert@tamu.edu>
2026-02-16 21:41:43 -0600

    Added header for database table Drink and 15 drink varieties

--- drink.py
+++ drink.py
@@ -1,1 +1,28 @@
-# This is dedicated to the Most Sacred Heart of Jesus, the Immaculate Heart of Mary and the Chaste Heart of St. Joseph
+'''
+This is dedicated to the Most Sacred Heart of Jesus, 
+the Immaculate Heart of Mary 
+and the Chaste Heart of St. Joseph
+'''
+import csv
+
+header = ['id', 'name', 'amount', 'ice_level', 'traditional_boba', 'popping_boba', 'sweetness', 'milk']
+drink_varieties = [
+    [1, 'Black Milk Tea', 6.00, 'Normal', False, False, 100, 'Cow']
+    [2, 'Green Milk Tea', 6.00, 'Normal', False, False, 100, 'Cow']
+    [3, 'Matcha Milk Tea', 6.00, 'Normal', False, False, 100, 'Cow']
+    [4, 'Taro Milk Tea', 6.00, 'Normal', False, False, 100, 'Cow']
+    [5, 'Jasmine Milk Tea', 6.00, 'Normal', False, False, 100, 'Cow']
+    [6, 'Oolong Milk Tea', 6.00, 'Normal', False, False, 100, 'Cow']
+    [7, 'Thai Milk Tea', 6.00, 'Normal', False, False, 100, 'Cow']
+    [8, 'Black Tea', 4.00, 'Normal', False, False, 100, 'None']
+    [9, 'Green Tea', 4.00, 'Normal', False, False, 100, 'None']
+    [10, 'Matcha Tea', 4.00, 'Normal', False, False, 100, 'None']
+    [11, 'Taro Tea', 4.00, 'Normal', False, False, 100, 'None']
+    [12, 'Jasmine Tea', 4.00, 'Normal', False, False, 100, 'None']
+    [13, 'Oolong Tea', 4.00, 'Normal', False, False, 100, 'None']
+    [14, 'Thai Tea', 4.00, 'Normal', False, False, 100, 'None']
+    [15, 'Chocolate Milk', 2.00, 'None', False, False, 100, 'Cow']
+]
+
+
+
