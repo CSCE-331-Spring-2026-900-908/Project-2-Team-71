0bed960bf4827233d26079d00282d9933c21a5be
Firestormatrix <111524418+Firestormatrix@users.noreply.github.com>
2026-02-17 06:47:29 -0600

    removed manual id  and coded the csv

--- cashier.py
+++ cashier.py
@@ -1,12 +1,18 @@
-employee_info = {
-     {"id": "C001", "name": "Marcus Chen", "phone": "555-0101", "hourly_pay": 12.50},
-     {"id": "C002", "name": "Sarah Jenkins", "phone": "555-0102", "hourly_pay": 12.50},
-     {"id": "C003", "name": "Amina Yusuf", "phone": "555-0103", "hourly_pay": 12.50},
-     {"id": "C004", "name": "David Miller", "phone": "555-0104", "hourly_pay": 12.50},
-     {"id": "C005", "name": "Elena Rodriguez", "phone": "555-0105", "hourly_pay": 12.50},
-     {"id": "C006", "name": "Kevin Park", "phone": "555-0106", "hourly_pay": 12.50},
-     {"id": "C007", "name": "Riley Thompson", "phone": "555-0107", "hourly_pay": 12.50},
-     {"id": "C008", "name": "Sloane Baxter", "phone": "555-0108", "hourly_pay": 12.50},
-     {"id": "C009", "name": "Isaac Newton", "phone": "555-0109", "hourly_pay": 12.50},
-     {"id": "C010", "name": "Maya Williams", "phone": "555-0110", "hourly_pay": 12.50}
-}
+employee_info = [
+     { "name": "Marcus Chen", "phone": "832-555-0101", "hourly_pay": 12.50},
+     { "name": "Sarah Jenkins", "phone": "832-555-0102", "hourly_pay": 12.50},
+     { "name": "Amina Yusuf", "phone": "832-555-0103", "hourly_pay": 12.50},
+     { "name": "David Miller", "phone": "832-555-0104", "hourly_pay": 12.50},
+     { "name": "Elena Rodriguez", "phone": "832-555-0105", "hourly_pay": 12.50},
+     { "name": "Kevin Park", "phone": "832-555-0106", "hourly_pay": 12.50},
+     { "name": "Riley Thompson", "phone": "832-555-0107", "hourly_pay": 12.50},
+     { "name": "Sloane Baxter", "phone": "832-555-0108", "hourly_pay": 12.50},
+     { "name": "Isaac Newton", "phone": "832-555-0109", "hourly_pay": 12.50},
+     { "name": "Maya Williams", "phone": "832-555-0110", "hourly_pay": 12.50}
+]
+
+with open("cashier.csv", "w") as file:
+    file.write("id, name, phone, hourly_pay")
+    for (id, employee) in enumerate(employee_info, start=1):
+        file.write(f"\n{id}, {employee['name']}, {employee['phone']}, {employee['hourly_pay']}")
+        file.flush()
