b31063ab6343a72cdfc80b009626d6f37461220f
Firestormatrix <111524418+Firestormatrix@users.noreply.github.com>
2026-02-17 21:56:33 -0600

    tester code

--- recipt.py
+++ recipt.py
@@ -4,24 +4,24 @@ from datetime import datetime, timedelta
 receipt_info = []
 
 # Setup for randomization
-cashier_ids = ["CASH-101", "CASH-102", "CASH-103", "CASH-104"]
-member_pool = [f"MEMB-{i}" for i in range(5000, 6000)]
+cashier_ids = [f"{j}" for j in range(0,10)]
+member_pool = [f"{i}" for i in range(0, 48)]#
 
 # Start date for random generation (last 30 days)
 base_date = datetime.now()
 
 for i in range(1, 301): # Loop 300 times
     # 1. Generate Unique Receipt ID (e.g., REC-1001)
-    receipt_id = f"REC-{1000 + i}"
+    receipt_id = f"{1000 + i}"
     
     # 2. Random price between 7.00 and 30.00
     price = round(random.uniform(7.00, 30.00), 2)
     
     # 3. 40% chance of being a member
-    cust_id = random.choice(member_pool) if random.random() < 0.4 else None
+    cust_id = random.choice(member_pool) if random.random() < 0.4 else "Anonymous User"
     
     # 4. Random date within the last month
-    random_days = random.randint(0, 30)
+    random_days = random.randint(0, 365)
     date_str = (base_date - timedelta(days=random_days)).strftime("%Y-%m-%d")
     
     # Add to our library
@@ -37,8 +37,8 @@ for i in range(1, 301): # Loop 300 times
 for r in receipt_info[:5]:
     print(r)
 # Write to CSV
-with open("receipt.csv", "w") as file:
-    file.write("receipt_id, purchase_date, total_price, customer_id, cashier_id\n")
-    for receipt in receipt_info:
-        line = f"{receipt['receipt_id']}, {receipt['purchase_date']}, {receipt['total_price']}, {receipt['customer_id']}, {receipt['cashier_id']}\n"
-        file.write(line)
+#with open("receipt.csv", "w") as file:
+#    file.write("receipt_id, purchase_date, total_price, customer_id, cashier_id\n")
+#    for receipt in receipt_info:
+#        line = f"{receipt['receipt_id']}, {receipt['purchase_date']}, {receipt['total_price']}, {receipt['customer_id']}, {receipt['cashier_id']}\n"
+#        file.write(line)
